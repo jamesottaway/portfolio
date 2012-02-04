523c618040a14b15e96e7530399473d9f9baa077
James Ottaway <james@ottaway.mp>
2012-02-04 20:42:13 +1000

    Wrote a photo view

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,16 @@ $: << File.dirname(__FILE__)+'/../lib'
 
 require 'portfolio'
 require 'pry'
+require 'slim'
 
 Dir[File.join(File.dirname(__FILE__),"../lib/portfolio/**/*.rb")].each { |f| require f }
 Dir[File.join(File.dirname(__FILE__),"../spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
     config.mock_with :rspec
+end
+
+def render template_path, locals={}
+  template = File.read(File.join(File.dirname(__FILE__), '../lib/portfolio/views', template_path))
+  Slim::Template.new() { template }.render(self, locals)
 end
---
+++ spec/views/photo.slim_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'photo.slim' do
+  let(:photo) { Photo.new 'title', 'src' }
+
+  subject { render 'photo.slim', :@photo => photo }
+
+  it { should include %Q{<title>#{photo.title}</title>} }
+  it { should include %Q{<h1 id="title">#{photo.title}</h1>} }
+  it { should include %Q{<img id="photo" src="#{photo.src}" />} }
+end
