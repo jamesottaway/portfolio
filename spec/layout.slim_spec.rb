20d561480a27e1f803d195ca66805b07d43f2b79
James Ottaway <james@ottaway.mp>
2012-02-15 10:03:36 +1000

    Added a spec for layout.slim to test passing a title in

--- spec/layout.slim_spec.rb
+++ spec/layout.slim_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe 'photo.slim' do
   let(:inner) { Proc.new {} }
-  
-  subject { render 'layout.slim', {}, inner }
-
+  subject { render 'layout.slim' }
   it { should include %Q{<title>James Ottaway</title>} }
+
+  describe 'custom title' do
+  	let(:title) { 'Custom Title' }
+	subject { render 'layout.slim', { :@title => title } }
+  	it { should include %Q{<title>#{title}</title>} }
+  end
 end
