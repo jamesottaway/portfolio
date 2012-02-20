228d6ac0d85b03b4e0121af470243ccd1ff85a04
James Ottaway <james@ottaway.mp>
2012-02-20 10:45:45 +1000

    Forgot to add the photo's ID as an attribute of the image element on the category page

--- spec/views/category.slim_spec.rb
+++ spec/views/category.slim_spec.rb
@@ -7,6 +7,6 @@ describe 'category.slim' do
   subject { render 'category.slim', :@category => category, :@photos => [photo] }
 
   it { should include %Q{<h1 id="title">#{category.title}</h1>} }
-  it { should include %Q{<img src="#{photo.src}" />} }
+  it { should include %Q{<img id="#{photo.id}" src="#{photo.src}" />} }
   it { should include %Q{<a href="/#{photo.id}">#{photo.title}</a>} }
 end
