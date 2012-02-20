e47729141d54945e076ae9de477833f1d920f941
James Ottaway <james@ottaway.mp>
2012-02-20 10:21:45 +1000

    Category view now takes an array of photos and displays some basic information for them

--- spec/views/category.slim_spec.rb
+++ spec/views/category.slim_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe 'category.slim' do
   let(:category) { Portfolio::Category.new 'Title' }
+  let(:photo) { mock 'Photo', :title => 'Cool Photo', :id => 'cool', :src => 'http://cool/photo/' }
 
-  subject { render 'category.slim', :@category => category }
+  subject { render 'category.slim', :@category => category, :@photos => [photo] }
 
   it { should include %Q{<h1 id="title">#{category.title}</h1>} }
+  it { should include %Q{<img src="#{photo.src}" />} }
+  it { should include %Q{<a href="/#{photo.id}">#{photo.title}</a>} }
 end
