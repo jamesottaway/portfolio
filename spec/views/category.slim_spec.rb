d34e8cceaaba3457aee312f263ab91d0b8648d26
James Ottaway <james@ottaway.mp>
2012-03-02 11:29:51 +1000

    Thumbnails now show correctly on the category pages

--- spec/views/category.slim_spec.rb
+++ spec/views/category.slim_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'category.slim' do
   let(:category) { Portfolio::Category.new 'Title' }
-  let(:photo) { mock 'Photo', :title => 'Cool Photo', :id => 'cool', :src => 'http://cool/photo/' }
+  let(:photo) { mock 'Photo', :title => 'Cool Photo', :id => 'cool', :thumb => 'http://cool/photo/' }
 
   subject { render 'category.slim', :@category => category, :@photos => [photo] }
 
   it { should include %Q{<h1 id="title">#{category.title}</h1>} }
-  it { should include %Q{<img id="#{photo.id}" src="#{photo.src}" />} }
+  it { should include %Q{<img id="#{photo.id}" src="#{photo.thumb}" />} }
   it { should include %Q{<a href="/#{photo.id}">#{photo.title}</a>} }
 end
