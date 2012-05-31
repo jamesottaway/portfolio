7a63484c8368ced0eed00fe71722da0cd890e455
James Ottaway <james@ottaway.mp>
2012-06-01 09:16:43 +1000

    Removing a bunch of Sinatra stuff now that I'm going to make it a front-end JS app

--- features/portfolio.feature
+++ features/portfolio.feature
@@ -1,3 +1,4 @@
+@wip
 Feature:
   As a photographer
   I want an online portfolio
--- lib/portfolio/server.rb
+++ lib/portfolio/server.rb
@@ -1,35 +1,8 @@
 $: << File.dirname(__FILE__)+'/..'
 
 require 'sinatra/base'
-require 'slim'
-require 'portfolio/portfolio'
 
 module Portfolio
   class Server < Sinatra::Base
-    set :root, File.dirname(__FILE__)+'/../..'
-    set :views, File.dirname(__FILE__)+'/views'
-
-    before do
-      @portfolio = Portfolio.new
-      @categories = @portfolio.categories
-    end
-
-    get '/' do
-      @photos = @portfolio.photos
-      slim :home
-    end
-
-    get '/:id' do
-      @photo = @portfolio.find_photo_by_id(params[:id])
-      @title = @photo.title
-      slim :photo
-    end
-
-    get '/category/:slug' do
-      @category = @portfolio.find_category_by_slug(params[:slug])
-      @title = @category.title
-      @photos = @portfolio.find_photos_by_category @category.title
-      slim :category
-    end
   end
 end
