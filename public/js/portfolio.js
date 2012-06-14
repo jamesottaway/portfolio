6a4281ae0a14925b16dd4047de83580fde846948
James Ottaway <james@ottaway.mp>
2012-06-14 22:25:29 +1000

    Inject first photo from portfolio into the DOM and style it nicely

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -3,3 +3,13 @@ var portfolio = {};
 var init = function(json) {
 	portfolio = json;
 }
+
+$(function() {
+	var photo = $('#photo');
+	var img = photo.children('img');
+	photo.css('background-image', 'url(/images/' + portfolio.photos[0].bg + ')');
+	img.attr('src', '/images/' + portfolio.photos[0].src);
+	img.load(function() {
+		$(this).css('padding-top', (photo.height() - img.height()) / 2);
+	});
+});
