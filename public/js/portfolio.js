41c0cf0423990189cb228b90d5f446e23d3bbca5
James Ottaway <james@ottaway.mp>
2012-07-11 14:02:17 +1000

    Limit the logic wrapped in document.ready

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,29 +1,32 @@
+var id = 0;
 var portfolio = {};
 
 var init = function(json) {
-	portfolio = json;
+  portfolio = json;
 }
 
-$(function() {
-	var show = function(photo) {	
-		$('#photo').css('background-image', 'url(/images/' + photo.bg + ')');
-		$('#photo > img').attr('src', '/images/' + photo.src);
-		$('#photo > img').load(function() {
-			$(this).css('padding-top', ($('#photo').height() - $('#photo > img').height()) / 2);
-		});
-	}
+var show = function(photo) {
+  $('#photo').css('background-image', 'url(/images/' + photo.bg + ')');
+  $('#photo > img').attr('src', '/images/' + photo.src);
+  $('#photo > img').load(function() {
+    $(this).css('padding-top', ($('#photo').height() - $('#photo > img').height()) / 2);
+  });
+}
 
-	var id = 0;
+var wireClick = function() {
+  $('#photo > img').click(function() {
+    if (id == '0') 
+      id = 1;
+    else
+      id = 0;
 
-	show(portfolio.photos[id]);
+    show(portfolio.photos[id]);
+  });
+}
 
-	$('#photo > img').click(function() {
-		if (id == '0') 
-			id = 1;
-		else
-			id = 0;
-
-		show(portfolio.photos[id]);
-	});
-});
-
+var run = function() {
+  $(function() {
+    show(portfolio.photos[id]);
+    wireClick();
+  });
+}
