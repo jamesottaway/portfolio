70db140aee4d06455d6ddd418fbf0ac8d9570e3c
James Ottaway <james@ottaway.mp>
2012-07-11 14:39:16 +1000

    Animate the transition between photos

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -5,11 +5,14 @@ var init = function(json) {
   portfolio = json;
 }
 
-var show = function(photo) {
+var show = function(photo, callback) {
   $('#photo').css('background-image', 'url(/images/' + photo.bg + ')');
   $('#photo > img').attr('src', '/images/' + photo.src);
   $('#photo > img').load(function() {
     $(this).css('padding-top', ($('#photo').height() - $('#photo > img').height()) / 2);
+    if (typeof(callback) == typeof(Function)) {
+      callback();
+    }
   });
 }
 
@@ -20,8 +23,22 @@ var wireClick = function() {
     else
       id = 0;
 
-    show(portfolio.photos[id]);
+    fade(function() {
+      show(portfolio.photos[id], function() {
+        fade();
+      });
+    });
   });
+}
+
+var fade = function(callback) {
+  var opacity;
+  if ($('#photo').css('opacity') == "1") {
+    opacity = 0;
+  } else {
+    opacity = 1;
+  }
+  $('#photo').fadeTo('fast', opacity, callback);
 }
 
 var run = function() {
